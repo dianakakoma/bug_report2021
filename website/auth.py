6cf0cc1fa0f395472f5ea3ef3de60aba4c00c397
dianakakoma <dikakoma@gmail.com>
2021-02-18 23:20:19 -0600

    sign-up page completed. Successful account creation. Note: navigation button for small screens isnot working.

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request, flash, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
@@ -6,15 +6,35 @@ auth = Blueprint('auth', __name__)
 def home():
     return render_template("home.html", text="testing")
 
-@auth.route('/login')
+@auth.route('/login', methods=["GET","POST"])
 def login():
+    data = request.form
+    print(data) #gives us the data submitted on th form
     return render_template("login.html", text="testing")
 
 @auth.route('/logout')
 def logout():
     return "<p>You are logged out!</p>"
-    
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=["GET","POST"])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        #form input restrictions
+        if len(email) < 10:
+            flash("email must be greater than 2 characters", category="error")
+        elif len(firstName) < 2:
+            flash("Your first name must be greater than 2 characters", category="error")
+        elif password1 != password2:
+            flash("The password confirmation does not match the initial password", category="error")
+        elif len(password1) < 5:
+            flash("Your password must be greater than 2 characters", category="error")
+            pass
+        else:
+            flash("Account created!", 'success')
+
     return render_template("sign_up.html")
