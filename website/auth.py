20329c60f54883a649c93019fc20abe92fdd82a6
dianakakoma <dikakoma@gmail.com>
2021-02-20 09:43:57 -0600

    I had forgotten to import the db earlier, but now I have, and I can succcessfully create new users.

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template,request, flash, redirect, url_for
 from .models import User
+from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
